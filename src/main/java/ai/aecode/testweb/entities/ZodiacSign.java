05d8d39bc9349af88f0ede351ca797e82e3a85bb
ManuelAecode <manuel.genplus@gmail.com>
2024-06-27 11:30:49 -0500

    update Zodiac table
    se actualizó el formato de las fechas

--- src/main/java/ai/aecode/testweb/entities/ZodiacSign.java
+++ src/main/java/ai/aecode/testweb/entities/ZodiacSign.java
@@ -4,6 +4,7 @@ import com.fasterxml.jackson.annotation.JsonFormat;
 import jakarta.persistence.*;
 
 import java.time.LocalDate;
+import java.time.MonthDay;
 import java.util.Date;
 
 @Entity
@@ -15,16 +16,16 @@ public class ZodiacSign {
     @Column(name = "sign_name")
     private String sign_name;
     @Column(name="sign_startdate")
-    @JsonFormat(pattern = "dd-MM-yyyy")
-    private LocalDate sign_startdate;
+    @JsonFormat(pattern = "dd-MM")
+    private MonthDay sign_startdate;
     @Column(name="sign_enddate")
-    @JsonFormat(pattern = "dd-MM-yyyy")
-    private LocalDate sign_enddate;
+    @JsonFormat(pattern = "dd-MM")
+    private MonthDay sign_enddate;
 
     public ZodiacSign() {
     }
 
-    public ZodiacSign(int id_zodiac, String sign_name, LocalDate sign_startdate, LocalDate sign_enddate) {
+    public ZodiacSign(int id_zodiac, String sign_name, MonthDay sign_startdate, MonthDay sign_enddate) {
         this.id_zodiac = id_zodiac;
         this.sign_name = sign_name;
         this.sign_startdate = sign_startdate;
@@ -47,19 +48,19 @@ public class ZodiacSign {
         this.sign_name = sign_name;
     }
 
-    public LocalDate getSign_startdate() {
+    public MonthDay getSign_startdate() {
         return sign_startdate;
     }
 
-    public void setSign_startdate(LocalDate sign_startdate) {
+    public void setSign_startdate(MonthDay sign_startdate) {
         this.sign_startdate = sign_startdate;
     }
 
-    public LocalDate getSign_enddate() {
+    public MonthDay getSign_enddate() {
         return sign_enddate;
     }
 
-    public void setSign_enddate(LocalDate sign_enddate) {
+    public void setSign_enddate(MonthDay sign_enddate) {
         this.sign_enddate = sign_enddate;
     }
 }
